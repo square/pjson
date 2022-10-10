c433e7fb8d28ae329c6a70279244fddd17f078b1
Seb <sebastien@squareup.com>
2022-10-10 09:54:15 -0700

    explain

--- src/FromJsonData.php
+++ src/FromJsonData.php
@@ -3,5 +3,14 @@ namespace Square\Pjson;
 
 interface FromJsonData
 {
+    /**
+     * Returns an instance of the class based on the received json data.
+     * The data received could be an array, an object (stdClass), a string, a bool, int or float (all standard json
+     * types)
+     *
+     * @param mixed $jd
+     * @param array $path if given, the path in the json value where the actual value is expected to be found
+     * @return static
+     */
     public static function fromJsonData($jd, array|string $path = []) : static;
 }
--- src/ToJsonData.php
+++ src/ToJsonData.php
@@ -3,5 +3,10 @@ namespace Square\Pjson;
 
 interface ToJsonData
 {
+    /**
+     * Returns data that can then be serialized via json_encode.
+     *
+     * @return mixed
+     */
     public function toJsonData();
 }
